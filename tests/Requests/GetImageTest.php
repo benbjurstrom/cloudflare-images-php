cb9accdc713e381124a9da02bd6d532ae2860445
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2023-03-03 17:40:32 -0800

    wip

--- tests/Requests/GetImageTest.php
+++ tests/Requests/GetImageTest.php
@@ -20,13 +20,8 @@ test('get image endpoint', function () {
     /* @var ImageData $data */
     $data = $response->dtoOrFail();
 
-    $signingKey = getenv('CLOUDFLARE_IMAGES_SIGNING_KEY');
-    $data->signUrls($signingKey);
-
     expect($response->ok())
         ->toBeTrue()
         ->and($data->id)
-        ->toBe('00000000-0000-0000-0000-000000000000')
-        ->and($data->variants[0])
-        ->toContain('https://imagedelivery.net/2222222222222222222222/00000000-0000-0000-0000-000000000000/public?sig=');
+        ->toBe('00000000-0000-0000-0000-000000000000');
 });
